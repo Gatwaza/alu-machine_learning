4627d6fd32bd2a7d4eb71a9400aa430e11309eda
Gatwaza <gatwazarobert177@gmail.com>
2023-09-09 20:21:05 +0200

    one * one give more

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+def mat_mul(mat1, mat2):
+    """
+    Performs matrix multiplication.
+
+    Args:
+        mat1 (list): The first 2D matrix.
+        mat2 (list): The second 2D matrix.
+
+    Returns:
+        list: A new 2D matrix containing the result of the matrix multiplication.
+              If the two matrices cannot be multiplied, returns None.
+    """
+    if len(mat1[0]) != len(mat2):
+        return None
+
+    result = [[sum(a*b for a, b in zip(row, col)) for col in zip(*mat2)] for row in mat1]
+    return result
+
+if __name__ == "__main__":
+    mat1 = [[1, 2],
+            [3, 4],
+            [5, 6]]
+    mat2 = [[1, 2, 3, 4],
+            [5, 6, 7, 8]]
+    print(mat_mul(mat1, mat2))
