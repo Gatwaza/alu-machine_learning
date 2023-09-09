dadbd5687c8689ac55db59e9654731bca09de2c6
Gatwaza <gatwazarobert177@gmail.com>
2023-09-09 08:31:50 -0700

    function to calculate matrix

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
+# Define the function matrix_shape
 def matrix_shape(matrix):
+    """
+    Calculates the shape of a matrix.
+
+    Args:
+    matrix (list): The input matrix.
+
+    Returns:
+    list: A list of integers representing the shape of the matrix.
+    """
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
