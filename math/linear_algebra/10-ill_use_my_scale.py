448ed8877f629c4a423a1d11eb85957046420023
Gatwaza <gatwazarobert177@gmail.com>
2023-09-20 19:00:57 +0200

    shape counts in matrix

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+This module defines a function to calculate the shape of a numpy.ndarray.
+"""
+
+import numpy as np
+
+def np_shape(matrix):
+    """
+    Calculate the shape of a numpy.ndarray.
+
+    Args:
+        matrix (numpy.ndarray): Input array.
+
+    Returns:
+        tuple of int: A tuple representing the dimensions of the input array.
+
+    Example:
+        >>> matrix = np.array([[1, 2, 3], [4, 5, 6]])
+        >>> np_shape(matrix)
+        (2, 3)
+    """
+    return np.shape(matrix)
