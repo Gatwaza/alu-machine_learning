23b49a3d7f6375f0e0182fc77a1346f2cbfaf102
Gatwaza <gatwazarobert177@gmail.com>
2023-09-20 17:31:14 +0200

    Polynomial integration

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -2,42 +2,37 @@
 
 def poly_integral(poly, C=0):
     """
-    Calculates the integral of a polynomial.
+    Calculate the integral of a polynomial.
 
     Args:
         poly (list): List of coefficients representing a polynomial.
-        C (int, optional): Integration constant. Default is 0.
+                     Index of the list represents the power of x.
+                     Example: if f(x) = x^3 + 3x + 5, poly is equal to [5, 3, 0, 1].
+        C (int): Integration constant (default is 0).
 
     Returns:
         list: List of coefficients representing the integral of the polynomial.
+              The returned list is as small as possible.
+
+    Raises:
+        ValueError: If poly is not a list of integers or if C is not an integer.
 
     Example:
-        If f(x) = x^3 + 3x + 5, poly is equal to [5, 3, 0, 1]
-
-    Note:
-        - If a coefficient is a whole number, 
-        it should be represented as an integer.
-        - If poly or C are not valid, return None.
-        - The returned list should be as small as possible.
+        >>> poly_integral([5, 3, 0, 1], 2)
+        [0, 5, 1, 0, 0.5]
     """
-    if not isinstance(poly, list) or not all(isinstance(coeff, (int, float)) for coeff in poly) or not isinstance(C, int):
-        return None
+    if not isinstance(poly, list) or not all(isinstance(coeff, int) for coeff in poly):
+        raise ValueError("poly must be a list of integers")
+    if not isinstance(C, int):
+        raise ValueError("C must be an integer")
 
-    result = [C]
+    integral_coeffs = [C]
     for i, coeff in enumerate(poly):
-        if not isinstance(coeff, int):
-            coeff = round(coeff)
-        integral_coeff = coeff / (i + 1)
-        result.append(integral_coeff)
+        power = i + 1
+        integral_coeff = coeff / power if coeff % power == 0 else coeff / power
+        integral_coeffs.append(int(integral_coeff))
 
-    while result[-1] == 0 and len(result) > 1:
-        result.pop()
-
-    return result
-
+    return integral_coeffs
 
 # Example usage
-poly = [5, 3, 0, 1]
-C = 0
-integral_poly = poly_integral(poly, C)
-print(integral_poly)
+
