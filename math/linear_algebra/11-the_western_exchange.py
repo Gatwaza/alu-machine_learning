bcee55c324b9930d33a82c687b6add94132fb0c4
Gatwaza <gatwazarobert177@gmail.com>
2023-09-20 19:11:15 +0200

    ..

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def np_transpose(matrix):
+    """
+    Transpose the input matrix.
+
+    Args:
+        matrix (numpy.ndarray): Input array.
+
+    Returns:
+        numpy.ndarray: Transposed array.
+
+    Example:
+        >>> matrix = np.array([[1, 2, 3], [4, 5, 6]])
+        >>> np_transpose(matrix)
+        array([[1, 4],
+               [2, 5],
+               [3, 6]])
+    """
+    return np.transpose(matrix)
