73f5cf8bab18ce2227940aa796d2b344e7647c99
Gatwaza <gatwazarobert177@gmail.com>
2023-09-20 17:45:55 +0200

    Polynomial integration

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -2,37 +2,42 @@
 
 def poly_integral(poly, C=0):
     """
-    Calculate the integral of a polynomial.
+    Calculates the integral of a polynomial.
 
     Args:
         poly (list): List of coefficients representing a polynomial.
-                     Index of the list represents the power of x.
-                     Example: if f(x) = x^3 + 3x + 5, poly is equal to [5, 3, 0, 1].
-        C (int): Integration constant (default is 0).
+                     The index of the list represents the power of x that the coefficient belongs to.
+        C (int, optional): Integration constant. Default is 0.
 
     Returns:
-        list: List of coefficients representing the integral of the polynomial.
-              The returned list is as small as possible.
+        list: A new list of coefficients representing the integral of the polynomial.
 
     Raises:
-        ValueError: If poly is not a list of integers or if C is not an integer.
+        TypeError: If poly is not a list of integers or if C is not an integer.
 
     Example:
         >>> poly_integral([5, 3, 0, 1], 2)
-        [0, 5, 1, 0, 0.5]
+        [2, 0, 1.5, 0, 0.25]
     """
-    if not isinstance(poly, list) or not all(isinstance(coeff, int) for coeff in poly):
-        raise ValueError("poly must be a list of integers")
-    if not isinstance(C, int):
-        raise ValueError("C must be an integer")
+    if not isinstance(poly, list) or not all(isinstance(coef, (int, float)) for coef in poly):
+        raise TypeError("poly must be a list of integers or floats")
 
-    integral_coeffs = [C]
-    for i, coeff in enumerate(poly):
-        power = i + 1
-        integral_coeff = coeff / power if coeff % power == 0 else coeff / power
-        integral_coeffs.append(int(integral_coeff))
+    if not isinstance(C, (int, float)):
+        raise TypeError("C must be an integer or float")
 
-    return integral_coeffs
+    result = [C]
+    for i, coef in enumerate(poly):
+        exponent = i + 1
+        new_coef = coef / exponent if exponent > 0 else 0
+        result.append(new_coef)
 
-# Example usage
+    # Remove trailing zeros
+    while result and result[-1] == 0:
+        result.pop()
 
+    return result
+
+if __name__ == "__main__":
+    poly = [5, 3, 0, 1]
+    C = 0
+    print(poly_integral(poly, C))
