73736b06d210262b1a9a0fbe49b7e01b6a33e224
Gatwaza <gatwazarobert177@gmail.com>
2023-09-20 19:20:34 +0200

    ..

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -4,14 +4,16 @@ def np_transpose(matrix):
     Transpose the input matrix.
 
     Args:
-        matrix (list of lists): Input array.
+        matrix (numpy.ndarray): Input array.
 
     Returns:
-        list of lists: Transposed array.
+        numpy.ndarray: Transposed array.
 
     Example:
-        >>> matrix = [[1, 2, 3], [4, 5, 6]]
+        >>> matrix = np.array([[1, 2, 3], [4, 5, 6]])
         >>> np_transpose(matrix)
-        [[1, 4], [2, 5], [3, 6]]
+        array([[1, 4],
+               [2, 5],
+               [3, 6]])
     """
-    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+    return matrix.T
