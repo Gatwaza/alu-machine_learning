175986cece94efbccd930410c74459769ebb880a
Gatwaza <gatwazarobert177@gmail.com>
2023-09-22 10:55:34 +0200

    slices along specific axis

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -7,22 +7,29 @@ This module defines a function to slice a matrix along specific axes.
 
 def np_slice(matrix, axes={}):
     """
-    Slice a matrix along specific axes.
+    Slices a matrix along specific axes.
 
     Args:
         matrix (numpy.ndarray): Input matrix.
-        axes (dict): Dictionary where the key is an axis to slice along and the
-            value is a tuple representing the slice to make along that axis.
+        axes (dict): Dictionary where the key is an axis to slice along and
+                     the value is a tuple representing the slice to make along that axis.
 
     Returns:
         numpy.ndarray: Sliced matrix.
-
-    Example:
-        >>> matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-        >>> axes = {0: (0, 2), 1: (1, 3)}
-        >>> np_slice(matrix, axes)
-        array([[2, 3],
-               [5, 6]])
     """
-    return matrix[tuple(slice(start, end)
-                        for axis, (start, end) in axes.items())]
+    # Copy the original matrix to avoid modifying it
+    result = [row[:] for row in matrix]
+
+    # Apply the specified slices along the specified axes
+    for axis, slice_range in axes.items():
+        if len(slice_range) == 1:
+            slice_range = slice_range[0]
+            if slice_range is not None:
+                result = result[slice(slice_range)]
+        elif len(slice_range) == 2:
+            start, stop = slice_range
+            if start is not None or stop is not None:
+                result = [row[start:stop] for row in result]
+
+    return result
+
