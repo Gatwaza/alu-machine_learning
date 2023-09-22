c50f03d02960869bee7ff33eac1481d5a249a5c4
Gatwaza <gatwazarobert177@gmail.com>
2023-09-22 10:13:23 +0200

    integrate x stuff

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -10,16 +10,16 @@ def poly_integral(poly, C=0):
 
     Args:
         poly (list): List of coefficients representing a polynomial.
-                    The index of the list represents. 
+                    The index of the list represents.
                     the power of x that the coefficient belongs to.
         C (int, optional): Integration constant. Default is 0.
 
     Returns:
-        list: A new list of coefficients representing, 
+        list: A new list of coefficients representing,
             the integral of the polynomial.
 
     Raises:
-        TypeError: If poly is not a list or if any coefficient is not 
+        TypeError: If poly is not a list or if any coefficient is not
         an integer or float, or if C is not an integer.
 
     Example:
