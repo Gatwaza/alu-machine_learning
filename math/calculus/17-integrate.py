1d9f061f743962cd364564956f79bd4e29619714
Gatwaza <gatwazarobert177@gmail.com>
2023-09-22 09:12:01 +0200

    integrate that x stuff

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+Function that calculates integral of polynomial.
+"""
+
 
 def poly_integral(poly, C=0):
     """
@@ -6,11 +10,13 @@ def poly_integral(poly, C=0):
 
     Args:
         poly (list): List of coefficients representing a polynomial.
-                     The index of the list represents the power of x that the coefficient belongs to.
+                     The index of the list represents, 
+                     the power of x that the coefficient belongs to.
         C (int, optional): Integration constant. Default is 0.
 
     Returns:
-        list: A new list of coefficients representing the integral of the polynomial.
+        list: A new list of coefficients representing, 
+              the integral of the polynomial.
 
     Raises:
         TypeError: If poly is not a list of integers or if C is not an integer.
@@ -19,28 +25,14 @@ def poly_integral(poly, C=0):
         >>> poly_integral([5, 3, 0, 1], 2)
         [2, 0, 1.5, 0, 0.25]
     """
-    if not isinstance(poly, list) or not all(isinstance(coef, (int, float)) for coef in poly):
-        raise TypeError("poly must be a list of integers or floats")
+    if not isinstance(poly, list) or \
+       not all(isinstance(coef, (int, float)) for coef in poly) or \
+       not isinstance(C, (int, float)):
+        return None
 
-    if not isinstance(C, (int, float)):
-        raise TypeError("C must be an integer or float")
+    integral_coeffs = [C]
+    for i, coef in enumerate(poly, start=1):
+        new_coef = coef / i
+        integral_coeffs.append(new_coef)
 
-    result = [C]
-    for i, coef in enumerate(poly):
-        exponent = i + 1
-        new_coef = coef / exponent if exponent > 0 else 0
-        result.append(new_coef)
-
-    # Remove trailing zeros
-    while result and result[-1] == 0:
-        result.pop()
-
-    # Handle formatting for integers
-    formatted_result = ['{:.1f}'.format(x) if isinstance(x, float) else int(x) for x in result]
-
-    return formatted_result
-
-if __name__ == "__main__":
-    poly = [5, 3, 0, 1]
-    C = 0
-    print(poly_integral(poly, C))
+    return integral_coeffs
