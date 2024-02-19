83e51aa0666bf3ff9383f00cdffca93162fc994a
Gatwaza <j.gatwaza@alustudent.com>
2024-02-19 02:14:54 -0800

    cost of using logistic regression

--- supervised_learning/classification/3-neuron.py
+++ supervised_learning/classification/3-neuron.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Defines a single neuron performing binary classification"""
+
 import numpy as np
 
-
 class Neuron:
-    """Neuron class"""
+    """Neuron class performing binary classification"""
 
     def __init__(self, nx):
         """Class constructor
@@ -21,8 +21,9 @@ class Neuron:
                 initialized using a random normal distribution.
             __b: The bias for the neuron, initialized to 0.
             __A: The activated output of the neuron (prediction),
-                 initialized to 0.
+                initialized to 0.
         """
+
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
@@ -31,34 +32,6 @@ class Neuron:
         self.__W = np.random.randn(nx).reshape(1, nx)
         self.__b = 0
         self.__A = 0
-
-    def forward_prop(self, X):
-        """Calculates the forward propagation of the neuron
-
-        Args:
-            X (numpy.ndarray): Input data with shape (nx, m).
-
-        Returns:
-            numpy.ndarray: The activated output (__A) of the neuron.
-        """
-        z = np.dot(self.__W, X) + self.__b
-        self.__A = 1 / (1 + np.exp(-z))
-        return self.__A
-
-    def cost(self, Y, A):
-        """Calculates the cost of the model using logistic regression
-
-        Args:
-            Y (numpy.ndarray): Correct labels with shape (1, m).
-            A (numpy.ndarray): Activated output of the neuron
-                                 with shape (1, m).
-
-        Returns:
-            float: The cost of the model.
-        """
-        m = Y.shape[1]
-        cost = -np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)) / m
-        return cost
 
     @property
     def W(self):
@@ -75,18 +48,47 @@ class Neuron:
         """Getter function for __A"""
         return self.__A
 
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neuron.
+
+        Args:
+            X (numpy.ndarray): Input data with shape (nx, m),
+                               where nx is the number of input features
+                               and m is the number of examples.
+
+        Returns:
+            numpy.ndarray: The activated output of the neuron.
+        """
+
+        Z = np.dot(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """Calculates the cost of the model using logistic regression.
+
+        Args:
+            Y (numpy.ndarray): Correct labels with shape (1, m).
+            A (numpy.ndarray): Activated output with shape (1, m).
+
+        Returns:
+            float: The cost of the model.
+        """
+
+        m = Y.shape[1]
+        cost = -np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)) / m
+        return cost
+
 
 if __name__ == "__main__":
-    import numpy as np
-
-    Neuron = __import__('3-neuron').Neuron
-
-    lib_train = np.load('../data/Binary_Train.npz')
-    X_3D, Y = lib_train['X'], lib_train['Y']
-    X = X_3D.reshape((X_3D.shape[0], -1)).T
-
-    np.random.seed(0)
-    neuron = Neuron(X.shape[0])
+    # Testing the Neuron class
+    neuron = Neuron(5)
+    X = np.array([[0.1, 0.2, 0.3],
+                  [0.4, 0.5, 0.6],
+                  [0.7, 0.8, 0.9],
+                  [0.2, 0.3, 0.4],
+                  [0.5, 0.6, 0.7]])
+    Y = np.array([[0, 1, 0]])
     A = neuron.forward_prop(X)
-    cost = neuron.cost(Y, A)
-    print(cost)
+    print(neuron.cost(Y, A))
+
