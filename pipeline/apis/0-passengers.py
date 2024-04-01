93ff62957d45c540baeee1b631e759e19d3d6f5a
Gatwaza <j.gatwaza@alustudent.com>
2024-04-01 12:12:04 +0200

    list ships

---
+++ pipeline/apis/0-passengers.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import requests
+
+def availableShips(passengerCount):
+    """
+    Retrieves a list of starships from the Star Wars API (Swapi) that can hold
+    a given number of passengers or more.
+
+    Args:
+        passengerCount (int): The minimum number of passengers a starship should be able to hold.
+
+    Returns:
+        list: A list of starship names that can hold at least the specified number of passengers.
+
+    """
+    url = 'https://swapi.dev/api/starships/'
+    ships = []
+
+    while url:
+        response = requests.get(url)
+        data = response.json()
+        
+        for ship in data['results']:
+            if ship['passengers'].isdigit() and int(ship['passengers']) >= passengerCount:
+                ships.append(ship['name'])
+
+        url = data['next']
+
+    return ships
+
+if __name__ == "__main__":
+    passengerCount = int(input("Enter the number of passengers: "))
+    print(availableShips(passengerCount))
