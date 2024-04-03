bb6234af4cb17243143b326eec8cfd57756b3566
Gatwaza <j.gatwaza@alustudent.com>
2024-04-03 20:37:53 +0200

    divide first with the second

--- pipeline/databases/21-div.sql
+++ pipeline/databases/21-div.sql
@@ -1,22 +1,11 @@
--- Create a stored procedure to compute the average score
--- for a user and update the average_score column in the users table.
-DELIMITER //
+-- Content: SQL to create a function that safely divides two numbers
+DELIMITER $$
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
 
-CREATE PROCEDURE ComputeAverageScoreForUser (
-    IN user_id_param INT
-)
 BEGIN
-    DECLARE user_avg_score FLOAT;
-    
-    -- Calculate average score for the user
-    SELECT AVG(score) INTO user_avg_score
-    FROM corrections
-    WHERE user_id = user_id_param;
-    
-    -- Update the average_score column in the users table
-    UPDATE users
-    SET average_score = user_avg_score
-    WHERE id = user_id_param;
-END //
-
-DELIMITER ;
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END $$
