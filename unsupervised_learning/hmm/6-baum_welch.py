4a120108a2a12e0812f5469332e955e2faa2d9f5
Gatwaza <gatwazarobert177@gmail.com>
2026-02-23 05:27:53 -0800

    the Baum-Welch algorithm for a hidden markov model

--- unsupervised_learning/hmm/6-baum_welch.py
+++ unsupervised_learning/hmm/6-baum_welch.py
@@ -30,56 +30,39 @@ def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
     M, N = Emission.shape
     T = Observations.shape[0]
 
-    if Transition.shape != (M, M):
-        return None, None
-    if Initial.shape != (M, 1):
+    if Transition.shape != (M, M) or Initial.shape != (M, 1):
         return None, None
 
     for _ in range(iterations):
         F = np.zeros((M, T))
         F[:, 0] = Initial[:, 0] * Emission[:, Observations[0]]
         for t in range(1, T):
-            F[:, t] = (
-                np.matmul(F[:, t - 1], Transition) *
-                Emission[:, Observations[t]]
-            )
+            F[:, t] = (np.matmul(F[:, t - 1], Transition) *
+                       Emission[:, Observations[t]])
 
         B = np.zeros((M, T))
         B[:, T - 1] = 1
         for t in range(T - 2, -1, -1):
             B[:, t] = np.matmul(
-                Transition,
-                Emission[:, Observations[t + 1]] * B[:, t + 1]
-            )
+                Transition, Emission[:, Observations[t + 1]] * B[:, t + 1])
 
-        # xi: shape (M, M, T-1)
-        # xi[i, j, t] = F[i,t] * T[i,j] * E[j, obs[t+1]] * B[j, t+1]
-        F_t = F[:, :T - 1]
-        B_t1 = B[:, 1:T]
-        E_t1 = Emission[:, Observations[1:T]]
+        a_t = F[:, :T - 1]
+        b_t1 = B[:, 1:T]
+        e_t1 = Emission[:, Observations[1:T]]
 
-        # numerator: (M, M, T-1)
-        xi = (F_t[:, np.newaxis, :] *
+        xi = (a_t[:, np.newaxis, :] *
               Transition[:, :, np.newaxis] *
-              (E_t1 * B_t1)[np.newaxis, :, :])
+              (e_t1 * b_t1)[np.newaxis, :, :])
+        xi /= xi.sum(axis=(0, 1), keepdims=True)
 
-        denom = np.sum(xi, axis=(0, 1), keepdims=True)
-        xi = xi / denom
-
-        gamma = np.sum(xi, axis=1)
-
-        # include last time step for gamma
-        last = np.sum(xi[:, :, T - 2], axis=1, keepdims=True)
+        gamma = xi.sum(axis=1)
+        last = xi[:, :, T - 2].sum(axis=1, keepdims=True)
         gamma_full = np.concatenate((gamma, last), axis=1)
 
-        # M-step: update Transition
-        Transition = (np.sum(xi, axis=2) /
-                      np.sum(gamma, axis=1, keepdims=True))
+        Transition = xi.sum(axis=2) / gamma.sum(axis=1, keepdims=True)
 
-        # M-step: update Emission
         for k in range(N):
-            mask = Observations == k
-            Emission[:, k] = (np.sum(gamma_full[:, mask], axis=1) /
-                              np.sum(gamma_full, axis=1))
+            Emission[:, k] = (gamma_full[:, Observations == k].sum(axis=1) /
+                              gamma_full.sum(axis=1))
 
     return Transition, Emission
