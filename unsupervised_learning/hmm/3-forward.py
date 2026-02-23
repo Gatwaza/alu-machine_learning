23d1d5a50e9fda25d4ddc4e6f27394523ba91f0c
Gatwaza <gatwazarobert177@gmail.com>
2026-02-23 05:01:48 -0800

    he forward algorithm for a hidden markov model

--- unsupervised_learning/hmm/3-forward.py
+++ unsupervised_learning/hmm/3-forward.py
@@ -38,7 +38,8 @@ def forward(Observation, Emission, Transition, Initial):
     F[:, 0] = Initial[:, 0] * Emission[:, Observation[0]]
 
     for t in range(1, T):
-        F[:, t] = np.matmul(F[:, t - 1], Transition) * Emission[:, Observation[t]]
+        transition_product = np.matmul(F[:, t - 1], Transition)
+        F[:, t] = transition_product * Emission[:, Observation[t]]
 
     P = np.sum(F[:, T - 1])
     return P, F
