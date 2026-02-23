28d591b3e03b9d2b728e80c5726f01a8b8f1dbd4
Gatwaza <gatwazarobert177@gmail.com>
2026-02-23 03:30:22 -0800

    he best number of clusters for a GMM using the Bayesian Information Criterion

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -39,7 +39,7 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
         kmax = n
     if not isinstance(kmax, int) or kmax <= 0:
         return None, None, None, None
-    if kmax < kmin:
+    if kmax <= kmin:
         return None, None, None, None
 
     results = []
