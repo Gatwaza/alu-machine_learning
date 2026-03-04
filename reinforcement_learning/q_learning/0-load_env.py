c38102852b7dd9ac1d837b65e3ae92de00348f4c
Gatwaza <gatwazarobert177@gmail.com>
2026-03-03 23:51:34 -0800

    loads pre-made environment

---
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module to load the FrozenLake environment from OpenAI gym"""
+import gym
+
+
+def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
+    """Loads the pre-made FrozenLakeEnv environment from OpenAI's gym.
+
+    Args:
+        desc: None or a list of lists containing a custom description
+              of the map to load for the environment
+        map_name: None or a string containing the pre-made map to load
+        is_slippery: boolean to determine if the ice is slippery
+
+    Returns:
+        the FrozenLake environment
+    """
+    env = gym.make('FrozenLake-v0', desc=desc, map_name=map_name,
+                   is_slippery=is_slippery)
+    return env
